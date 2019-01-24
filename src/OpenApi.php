61f7f7691b066f97e0a2b2b8735006ca03c602ce
wangzhongjie <baidushanxi@vip.qq.com>
2019-01-24 15:28:23 +0800

    add package source files.

---
+++ src/OpenApi.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzhongjie  Email: baidushanxi@vip.qq.com
+ * Date: 2018/12/7
+ * Time: 上午11:39
+ */
+namespace Baidushanxi\JrttMarketingApi;
+
+
+class OpenApi
+{
+
+    public function __construct()
+    {
+
+    }
+
+    
+
+
+
+
+}
